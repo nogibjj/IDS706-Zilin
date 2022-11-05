c0d8721180564ecdf7450fc2fa0cb245fc96039e
Jeffy <68449918+yzlhahaha@users.noreply.github.com>
2022-11-05 03:27:33 +0000

    update

--- script.py
+++ script.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sodapy import Socrata
 import os  # also need os
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+# import termplotlib as tpl
 
 load_dotenv()  # blank if .env file in same directory as script
 # load_dotenv('<path to file>.env') to point to another location
@@ -23,7 +25,7 @@ client = Socrata("data.cityofnewyork.us",
 # dictionaries by sodapy.
 # Motor Vehicle Collisions - Crashes DB from NYC open data
 count = client.get(
-    "h9gi-nx95", 
+    "h9gi-nx95",
     select="count(collision_id)",
     where="crash_date > '2014-10-28T00:00:00.000' AND number_of_persons_killed is not null AND number_of_persons_killed > 0",
 )
@@ -32,15 +34,37 @@ print(int(count[0]['count_collision_id']) / (8 * 365))
 
 records = client.get(
     "h9gi-nx95", 
-    limit=100,
     select="crash_date,crash_time,latitude,longitude,number_of_persons_injured,number_of_persons_killed",
-    where="crash_date > '2014-10-13T00:00:00.000' AND number_of_persons_killed is not null",
+    where="crash_date > '2014-10-13T00:00:00.000' AND number_of_persons_killed is not null AND latitude > 40 AND longitude > -75",
     order="number_of_persons_killed DESC, number_of_persons_injured DESC, crash_date DESC, crash_time DESC"
 )
 
 # Convert to pandas DataFrame
 df = pd.DataFrame.from_records(records)
-print(df)
+print(df.head)
+print(df.dtypes)
 
-# TODO find the relationship between crash_time and crashes
-# TODO find the relationship between latitude, longitude and crashes 
+df['crash_date'] = pd.to_datetime(df['crash_date'])
+df['crash_time'] = pd.to_datetime(df['crash_time'], format='%H:%M')
+df['latitude'] = df['latitude'].astype(float)
+df['longitude'] = df['longitude'].astype(float)
+df['number_of_persons_injured'] = df['number_of_persons_injured'].astype(int)
+df['number_of_persons_killed'] = df['number_of_persons_killed'].astype(int)
+
+print(df.dtypes)
+
+# find the relationship between crash_date and crashes
+x = df['crash_date']
+y = df['number_of_persons_killed']
+# plotting 
+plt.plot(x, y, 'bo')
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.savefig("date_crashes.png")
+plt.close()
+
+nx = df['latitude']
+ny = df['longitude']
+nc = df['number_of_persons_killed']
+plt.scatter(nx, ny, s=10, c=nc, cmap='gray')
+plt.savefig("location_crashes.png")
--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env python
 sudo apt-get update
+sudo apt-get install gnuplot
 pip install -r requirements.txt
